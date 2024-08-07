8abe66eb2ce6a3f97868a41d4d25bae0daf11b46
TalibY22 <hzhzhhsj58@gmail.com>
2024-08-07 20:52:14 +0300

    database_creation

---
+++ database/migrations/2024_08_07_173703_create_cart_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('CustomerID');
+            $table->unsignedBigInteger('ProductID');
+
+            $table->foreign('CustomerID')->references('id')->on('customers');
+            $table->foreign('ProductID')->references('id')->on('products');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart');
+    }
+};
